faa4575641185c6525b1bdc61d4a824b8dcf5e45
xiwenk <xiwenk188@gmail.com>
2025-07-05 20:37:16 +0800

    initial commit

---
+++ src/main/java/com/example/highconcurrencylikesystem/HighConcurrencyLikeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.highconcurrencylikesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyLikeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighConcurrencyLikeSystemApplication.class, args);
+    }
+
+}
